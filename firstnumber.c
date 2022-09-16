20964944bc555f6398b5d826bb70f63949825833
Dr. Frank Emanuel <emanuef@algonquincollege.com>
2022-09-16 11:21:11 -0400

    first part of lab.

---
+++ firstnumber.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    int number = 19;
+
+    printf("gimme a number: ");
+
+    scanf("%d", &number);
+
+    printf("your number = %d\n", number);
+
+    number=12;
+    
+    printf("your number = %d\n", number);
+
+
+    return 0;
+}
