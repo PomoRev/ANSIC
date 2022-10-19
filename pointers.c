f4590b51f49e4d6eef6256894c4b50acfa642974
Dr. Frank Emanuel <emanuef@algonquincollege.com>
2022-10-19 17:00:51 -0400

    pointer fun

--- pointers.c
+++ pointers.c
@@ -10,8 +10,14 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
+
+    struct person {
+        int age;
+        char gender;
+    };
 
 /*     basic variables are created on the stack and the compiler 
     gives us a standardized notation so that we do not have
@@ -24,6 +30,20 @@ int main(){
     does provide us with an alternative offset notation. */
 
     char aCString[] = "Hello World!";
+    int * pointer2String;
+    float aRealNumber = 4.12;
+
+    int * arrayOfNumbers[3];
+
+    struct person aPeople;
+
+    struct person peoples[] = {{ 12, 'g' }, { 34, 'p' }, { 62, 'm'}, { 62, 'm'}, 
+    { 62, 'm'}, { 8, 'm'}, { 9, 'm'}, { 10, 'm'}, { 48, 'm'}, { 62, 'm'} };
+
+    struct person * harold = peoples;
+
+    aPeople.age = 12;
+    aPeople.gender = 'n';
 
 /*     we can access their values like so */
 
@@ -32,9 +52,37 @@ int main(){
 /*     we can see their addresses like so */
 
     printf("%ld for the string and %ld for the integer.\n", 
-        (long int)&aCString, (long int)&aNumber);
+        (long int) & aCString, (long int) & aNumber);
 
-    return 0;
+    printf("%d size of string %d size of int\n", (int)sizeof(aCString), (int)sizeof(aNumber));
+
+    pointer2String = & aNumber;
+
+    printf("%ld the pointer uninitialized is this.\n", (long int)pointer2String);
+
+    printf("Harold is %d years old. The struct is %d bytes.\n", aPeople.age, (int)sizeof(aPeople) );
+
+    printf("Float size: %d \n", (int)sizeof(aRealNumber));
+
+    printf("second person age %d\n", peoples[1].age);
+
+    printf("second person age from pointer %d\n", harold->age );
+
+    (harold + sizeof(aPeople))->age = 50;
+
+    printf("really the second person's age %d\n", ( harold + sizeof(aPeople))->age );
+    
+    arrayOfNumbers[0] = calloc(1, sizeof(int));
+
+    * arrayOfNumbers[0] = 14;
+
+    printf(" %d oh yeah...\n", * arrayOfNumbers[0]);
+
+    free( arrayOfNumbers[0] );
+
+    printf(" %d oh yeah...\n", * arrayOfNumbers[0]);
+
+    return 14;
 }
 
 
