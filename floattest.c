671e0f51111f791ade5d94cace528d0a74f9a4fc
Dr. Frank Emanuel <emanuef@algonquincollege.com>
2022-11-11 10:32:17 -0500

    float?

---
+++ floattest.c
@@ -0,0 +1,19 @@
+/* floattest.c
+ * 
+ * Prof. Frank Emanuel, Ph.D. 
+ * CST8234
+ * 2022-10-20
+ */
+
+#include <stdio.h>
+
+int main(){
+
+    double numToTest = 4.67;
+
+    printf("%d is the mod of %f.", ((int)numToTest % 10), numToTest);
+
+    return 0;
+}
+
+
